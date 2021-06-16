8269051d605515c5330c5becfc335fb56e4f1f7e
Blazej Doleska <blazej.doleska@creativestyle.pl>
2021-06-16 11:33:54 +0200

    module configuration files

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MageSuite_SeoHreflangUrlTranslation',
+    __DIR__
+);
